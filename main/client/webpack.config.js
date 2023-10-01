e326dd8cfba8bde43640a5da83bfa235a633b309
AllenHirmiz <allenaliraqi@gmail.com>
2023-10-01 15:28:26 +1100

    update packages files code

--- main/client/webpack.config.js
+++ main/client/webpack.config.js
@@ -1,29 +1,96 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
+module.exports = (env, argv) => {
+  const injectManifest = new InjectManifest({
+    swSrc: "./src-sw.js",
+    swDest: "src-sw.js",
+    ...(argv.mode !== "production" ? { exclude: [/./] } : {}),
+  });
+
+  if (argv.mode !== "production") {
+    // In dev, suppress the "InjectManifest has been called multiple times" warning by reaching into
+    // the private properties of the plugin and making sure it never ends up in the state
+    // where it makes that warning.
+    Object.defineProperty(injectManifest, "alreadyCalled", {
+      get() {
+        return false;
+      },
+      set() {
+        // do nothing; the internals try to set it to true, which then results in a warning
+        // on the next run of webpack.
+      },
+    });
+  }
+
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      // Entry point for files
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      // editor: './src/editor.js'
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      // Output for our bundles
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        // Webpack plugin that generates our html file and injects our bundles.
+        template: "./index.html",
+        title: "J.A.T.E",
+      }),
+      InjectManifest,
+      new WebpackPwaManifest({
+        // Creates a manifest.json file.
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
+        description:
+          "Create notes and code snippets with or without an internet connection.",
+        background_color: "#f5f5f5",
+        theme_color: "#31a9e1",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: 96,
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
