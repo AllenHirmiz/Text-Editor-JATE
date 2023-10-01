29abdf6c4ad749a202b2aa9068ef7ed0ae5e7bdd
AllenHirmiz <allenaliraqi@gmail.com>
2023-10-01 15:46:39 +1100

    update client webpack packages

--- main/client/webpack.config.js
+++ main/client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -67,6 +68,9 @@ module.exports = (env, argv) => {
             destination: path.join("assets", "icons"),
           },
         ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
       }),
     ],
 
